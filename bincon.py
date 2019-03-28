3e875a22c3ab8d31b1edc665f8f92c95b5a03583
Josh <joshuamcdowell97@gmail.com>
2019-03-28 17:34:02 -0500

    added comments

--- bincon.py
+++ bincon.py
@@ -1,13 +1,14 @@
+# Program 1 - Binary Decoder
+# Team: Babylonians
+# Group Members: Martin Brock, John Chung, Casey Fernandez,William Francis,Joshua McDowell, Reid Naylor, Zachary Rogers
 #
-#
-#
-#
-#
+# Github Repository: https://github.com/joshuamcdowell/BinaryDecoder.git
 
 
 # PYTHON BINARY INTERPRETER
 import sys
 
+# Takes binary data, and chunk size and converts it into its ASCII characters.
 def make_string(binary, bitLength):
     sent = []
 
@@ -31,21 +32,23 @@ def make_string(binary, bitLength):
         sys.stdout.write(char)
     print
 
-
+# Removes whitespaces
 def clean(string):
     string = string.replace(' ', '')
     string = string.replace('\n', '')
     return string
 
 
-
+# Read the binary data from standard input and remove all whitespaces
 bi = sys.stdin.read()
 bi = clean(bi)
 
+# Check for input
 if(bi == ""):
     print("No Input Recieved")
     exit()
 
+# See if the binary data is divisible into chunks of 7 and/or 8 bits and then convert it to text respectively
 biLen = len(bi)
 if(biLen % 8 == 0 and biLen % 7 == 0):
     make_string(bi, 7)
